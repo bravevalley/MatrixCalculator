6e3fb4c3cb4321455ac773ff7968a79f7800bd62
Dassyareg <dassyareg@gmail.com>
2022-10-06 22:46:00 +0100

    Tested the formular

--- main.go
+++ main.go
@@ -2,19 +2,30 @@ package main
 
 import "fmt"
 
+const matrixSize = 3
+
 var (
-	matrixSize = 3
-	matrixA    = [][]int{{9, -4, -6},
+	matrixA = [matrixSize][matrixSize]int{{9, -4, -6},
 		{-5, 3, 2},
 		{1, -3, 2}}
 
-	matrixB = [][]int{{9, -3, 7},
+	matrixB = [matrixSize][matrixSize]int{{9, -3, 7},
 		{4, 1, -2},
 		{6, 5, -4}}
-
-	result [][]int
+	result [matrixSize][matrixSize]int
 )
 
 func main() {
-	
+
+	for row := 0; row < matrixSize; row++ {
+		for col := 0; col < matrixSize; col++ {
+			for i := 0; i < matrixSize; i++ {
+				result[row][col] += matrixA[row][i] * matrixB[col][i]
+
+			}
+		}
+	}
+
+	fmt.Printf("%-v\n", result)
+
 }
