4157053c7b70b9e2d4683f6edc90dd44c7a37464
Dassyareg <dassyareg@gmail.com>
2022-10-07 20:26:09 +0100

    created a composition

--- main.go
+++ main.go
@@ -1,36 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
-const matrixSize = 3
+const matrixSize = 4
 
 var (
-	matrixA = [matrixSize][matrixSize]int{{9, -4, -6},
-		{-5, 3, 2},
-		{1, -3, 2}}
+	matrixA = [matrixSize][matrixSize]int{}
 
-	matrixB = [matrixSize][matrixSize]int{{9, -3, 7},
-		{4, 1, -2},
-		{6, 5, -4}}
+	matrixB = [matrixSize][matrixSize]int{}
+
 	result [matrixSize][matrixSize]int
 )
 
-func main() {
+func preformatRow(row int) {
+	for col := 0; col < matrixSize; col++ {
+		for i := 0; i < matrixSize; i++ {
+			// Formular:
+			// [r1c1*r1c1+r1c2*r2c1         r1c1*r1c2+r1c2*r2c2]
+			// [r2c1*r1c+r2c2*r2c1          r2c2*r1c2+r2c2*r2c2]
 
-	for row := 0; row < matrixSize; row++ {
-		for col := 0; col < matrixSize; col++ {
-			for i := 0; i < matrixSize; i++ {
-				// Formular:
-				// [r1c1*r1c1+r1c2*r2c1         r1c1*r1c2+r1c2*r2c2]
-				// [r2c1*r1c+r2c2*r2c1          r2c2*r1c2+r2c2*r2c2]
-				
+			result[row][col] += matrixA[row][i] * matrixB[i][col]
 
-				result[row][col] += matrixA[row][i] * matrixB[i][col]
-
-			}
 		}
 	}
 
+}
+
+func randMatrixGenerator(matrix *[matrixSize][matrixSize]int) {
+	for row := 0; row < matrixSize; row++ {
+		for col := 0; col < matrixSize; col++ {
+
+			// Assign an element to each index of the matrix
+			matrix[row][col] = rand.Intn(10) - 5
+
+		}
+
+	}
+
+}
+
+func main() {
+
+	// Generate random numbers to fill the matrices
+	randMatrixGenerator(&matrixA)
+	randMatrixGenerator(&matrixB)
+
+	// Pass the row into the row function.
+	for row := 0; row < matrixSize; row++ {
+		preformatRow(row)
+
+	}
+
 	fmt.Printf("%-v\n", result)
+	fmt.Printf("%-v\n", matrixA)
+	fmt.Printf("%-v\n", matrixB)
 
 }
