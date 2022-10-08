c1bbd7767b1121bac5f425185fc812ff83525803
Dassyareg <dassyareg@gmail.com>
2022-10-08 14:23:29 +0100

    concurrency model

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
+	"time"
 )
 
 const matrixSize = 4
@@ -13,17 +15,29 @@ var (
 	matrixB = [matrixSize][matrixSize]int{}
 
 	result [matrixSize][matrixSize]int
+
+	rwLock = sync.RWMutex{}
+
+	condition = sync.NewCond(rwLock.RLocker())
+
+	wg = sync.WaitGroup{}
 )
 
 func preformatRow(row int) {
-	for col := 0; col < matrixSize; col++ {
-		for i := 0; i < matrixSize; i++ {
-			// Formular:
-			// [r1c1*r1c1+r1c2*r2c1         r1c1*r1c2+r1c2*r2c2]
-			// [r2c1*r1c+r2c2*r2c1          r2c2*r1c2+r2c2*r2c2]
 
-			result[row][col] += matrixA[row][i] * matrixB[i][col]
+	rwLock.RLock()
+	for {
+		wg.Done()
+		condition.Wait()
+		for col := 0; col < matrixSize; col++ {
+			for i := 0; i < matrixSize; i++ {
+				// Formular:
+				// [r1c1*r1c1+r1c2*r2c1         r1c1*r1c2+r1c2*r2c2]
+				// [r2c1*r1c+r2c2*r2c1          r2c2*r1c2+r2c2*r2c2]
 
+				result[row][col] += matrixA[row][i] * matrixB[i][col]
+
+			}
 		}
 	}
 
@@ -44,18 +58,26 @@ func randMatrixGenerator(matrix *[matrixSize][matrixSize]int) {
 
 func main() {
 
-	// Generate random numbers to fill the matrices
-	randMatrixGenerator(&matrixA)
-	randMatrixGenerator(&matrixB)
+	start := time.Now()
+	wg.Add(matrixSize)
 
 	// Pass the row into the row function.
 	for row := 0; row < matrixSize; row++ {
-		preformatRow(row)
+		go preformatRow(row)
 
 	}
 
-	fmt.Printf("%-v\n", result)
-	fmt.Printf("%-v\n", matrixA)
-	fmt.Printf("%-v\n", matrixB)
+	// Generate random numbers to fill the matrices
+	for i := 0; i < 100; i++ {
+		wg.Wait()
+		rwLock.Lock()
+		randMatrixGenerator(&matrixA)
+		randMatrixGenerator(&matrixB)
+		wg.Add(matrixSize)
+		rwLock.Unlock()
+		condition.Broadcast()
+	}
 
+	finish := time.Since(start)
+	fmt.Println(finish)
 }
